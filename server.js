63d47824107595e69f760d45bbf681d852f05961
JonnyRiver <jonnyriver6@gmail.com>
2022-04-03 20:52:01 -0400

    new commit

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker6";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -26,5 +25,5 @@ mongoose.connect(MONGODB_URI, {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!!!!!!!`);
 });
